a43c0d7be68b2531eaafed5f86629f9038f9e124
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-03-02 01:13:15 -0300

    fix: styles
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,17 +1,15 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
-const customStyle = {
-  tabSize: 2,
-  borderRadius: 'var(--radius)',
-}
-
 const Code = ({ children, language = 'javascript' }) => {
   return (
     <SyntaxHighlighter
       language={language.toLowerCase()}
       style={darcula}
-      customStyle={customStyle}
+      customStyle={{
+        tabSize: 2,
+        borderRadius: 'var(--radius)',
+      }}
     >
       {children}
     </SyntaxHighlighter>

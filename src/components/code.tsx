dcfa9e51b0782d581e7f02635167e52062d4974a
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-03-02 01:08:53 -0300

    fix: use prism
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,9 +1,18 @@
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+const customStyle = {
+  tabSize: 2,
+  borderRadius: 'var(--radius)',
+}
 
 const Code = ({ children, language = 'javascript' }) => {
   return (
-    <SyntaxHighlighter language={language.toLowerCase()} style={dracula}>
+    <SyntaxHighlighter
+      language={language.toLowerCase()}
+      style={darcula}
+      customStyle={customStyle}
+    >
       {children}
     </SyntaxHighlighter>
   )
